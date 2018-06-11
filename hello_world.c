5b65c9d3def5da6270209f07cbcd3e655c637e42
Ilya Kopyl <ilya.kopyl@gmail.com>
2018-06-10 19:58:42 -0700

    Added my own implementation of strlen function and completed the first half of the program

--- hello_world.c
+++ hello_world.c
@@ -1,13 +1,44 @@
 /**
  * CSC 415 Assignment 1
+ * Completed by Ilya Kopyl (ikopyl@mail.sfsu.edu)
 */
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define USERNAME "Ilya Kopyl"
 
 
+unsigned int mystrlen(const char*);
 
-int
-main(int argc,  char** argv)
+
+int main(int argc,  char** argv)
 {
+    static const char* template = "CSC 415, This program written by %s!!!\n";
+    const int buffer = mystrlen(template) - 2 + mystrlen(USERNAME);
+
+    char* out = malloc(buffer);
+    int numOfBytes = sprintf(out, template, USERNAME);
+
+    if (!numOfBytes || numOfBytes != buffer) { perror("sprintf error"); }
+
+
+    free(out);
 	return 0;
+}
+
+/**
+ * My own implementation of strlen function
+ * @param array of characters
+ * @return characters count
+ */
+unsigned int mystrlen(const char* array)
+{
+    unsigned int counter = 0;
+    char *p = (char *) array;
+    while (*p != '\0') {
+        counter++;
+        p++;
+    }
+    return counter;
 }
